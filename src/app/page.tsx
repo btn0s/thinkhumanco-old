699187f979ed46e84f15df64c434b7974a3dadc4
b <brendan.t.norris@gmail.com>
2023-11-28 19:09:28 -0700

    all

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,106 @@
+import { Metadata } from 'next'
 import Image from 'next/image'
+import { FC, PropsWithChildren } from 'react'
+import logoImg from '@/assets/images/logo.png'
+
+export const metadata: Metadata = {
+  title: 'thinkhuman.co',
+  description: 'We help mission-driven startups bootstrap and scale.',
+  openGraph: {
+    type: 'website',
+    url: 'https://thinkhuman.co',
+
+    title: 'We help mission-driven startups bootstrap and scale.',
+    description: `We're a group of product design and technology specialists working with mission-focused startups to solve problems that make an impact.`,
+    images: [
+      {
+        url: 'https://thinkhuman.co/images/og-share.png',
+        width: 1200,
+        height: 630,
+        alt: 'thinkhuman.co',
+      },
+    ],
+    siteName: 'thinkhuman.co',
+  },
+  twitter: {
+    site: 'thinkhuman.co',
+    card: 'summary_large_image',
+  },
+  themeColor: '#D1F0D5',
+}
+
+const SectionTitle: FC<PropsWithChildren> = ({ children }) => {
+  return <h3 className="text-2xl font-semibold">{children}</h3>
+}
+
+const SectionBody: FC<PropsWithChildren> = ({ children }) => {
+  return <p className="text-lg">{children}</p>
+}
+
+const Section: FC<PropsWithChildren> = ({ children }) => {
+  return <section className="flex flex-col gap-4">{children}</section>
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div className={'mx-auto flex max-w-4xl flex-col gap-24 px-6 py-24'}>
+      <Section>
+        <Image src={logoImg} alt={'thinkhuman.co'} width={48} height={48} />
+      </Section>
+      <Section>
+        <h1 className={'mb-4 text-4xl font-bold'}>
+          Supporting startups{' '}
+          <span className={'text-[#286D49]'}>serving humanity</span>
+        </h1>
+        <h2 className={'text-2xl'}>
+          We're a group of product design and technology specialists working
+          with mission-focused startups to solve problems that make an impact.
+        </h2>
+      </Section>
+      <Section>
+        <SectionTitle>Our mission-focused approach</SectionTitle>
+        <SectionBody>
+          Traditional fundraising often presents a dilemma for mission-driven
+          startups, caught between the need for capital and maintaining their
+          core values.
+        </SectionBody>
+        <SectionBody>
+          We're here to offer an alternative: We partner with you along the way,
+          providing the expertise you need to bootstrap and scale — all for the
+          rough cost of an early hire.
+        </SectionBody>
+      </Section>
+      <Section>
+        <SectionTitle>What makes us unique</SectionTitle>
+        <SectionBody>
+          Unlike traditional consulting firms or investors, we measure our
+          success by the real-world impact of the startups we assist. Our unique
+          value lies in our hands-on approach, offering not just advice but also
+          rolling up our sleeves and getting involved at the operational level.
+        </SectionBody>
+        <SectionBody>
+          We become a part of your team, sharing your challenges and celebrating
+          your victories as our own.
+        </SectionBody>
+      </Section>
+      <Section>
+        <SectionTitle>We're not for everyone</SectionTitle>
+        <SectionBody>
+          We seek out startups with a clear, compelling mission and the
+          potential to make a significant impact. A strong, committed founder
+          team, a product or service that solves a real problem, and a business
+          model that shows potential for scalability are what we find exciting.
+          We value transparency, resilience, and a willingness to learn and grow
+          together.
+        </SectionBody>
+      </Section>
+      <Section>
+        <SectionBody>
+          If you're looking for a partner to help you build a better future,
+          we'd love to hear from you.
+        </SectionBody>
+        <p className={'text-3xl font-bold'}>hello@thinkhuman.co</p>
+      </Section>
+    </div>
   )
 }
